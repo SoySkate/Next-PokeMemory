d3bb8ce4812d8ec64c6ecb91e86c990d1089aca9
Andrew <andreumartintorres.amt@gmail.com>
2022-11-11 18:54:11 +0100

    NextPokeEdit

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,10 @@ export default function Patata() {
     <div className="bg-gray-400 min-h-screen">
       <div className="flex bg-[url('../public/startMenu.jpg')] md:bg-[url('../public/menuPc.jpg')] md:py-96 bg-cover h-72 justify-center items-center">
         <div className="md:py-12 md:px-12 py-6 px-6 bg-red-600 border border-black  rounded-full shadow-xl">
-          <Link href="/playing">
-            <button className="md:text-3xl md:font-bold text-xl font-bold border-2 border-black  bg-white rounded-full md:py-10 md:px-6 py-4 px-2 hover:bg-slate-500 hover:text-white">
+          <Link href="/playing" className="md:text-3xl md:font-bold text-xl font-bold border-2 border-black  bg-white rounded-full md:py-10 md:px-6 py-4 px-2 hover:bg-slate-500 hover:text-white">
+      
               PLAY
-            </button>
+           
           </Link>
         </div>
       </div>
